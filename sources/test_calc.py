380aa68e15391c6a13248b73a7d98d7bdb3ff234
lakhwinder555 <lakhwindersinghwins@gmail.com>
2024-10-29 12:04:13 +0530

    Update test_calc.py

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -36,15 +36,6 @@ def test_add_string_and_integer(self):
         result = calc.add2('abc', 3)
         self.assertEqual(result, 'abc3')
 
-    def test_add_string_and_number(self):
-        """
-        Test the addition of a string and a float returns them as one
-        concatenated string (in which the float is converted to a string)
-        """
-        result = calc.add2('abc', '5.5')
-        self.assertEqual(result, 'abc5.5')
-
-if __name__ == '__main__':
-    unittest.main()
+   
 
     
